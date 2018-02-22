1638343a80eaf535f27ded957c1cabb0abb781e8
Nico <salvador.nico@gmail.com>
2018-02-22 23:05:37 +0800

    Set up router

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,16 @@
 import Vue from "vue"
 import Router from "vue-router"
+import Home from "@/components/Home.vue"
 
 Vue.use(Router)
 
 export default new Router({
+	mode: "history",
 	routes: [
-		// {
-		//   path: '/',
-		//   name: 'home',
-		//   component: Home
-		// },
-		// {
-		//   path: '/about',
-		//   name: 'about',
-		//   component: About
-		// }
+		{
+			path: "/",
+			name: "home",
+			component: Home,
+		},
 	],
 })
