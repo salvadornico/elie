e59c710152bb9dbe4538fb17d6826574657c6b20
Nico <salvador.nico@gmail.com>
2018-02-24 10:32:04 +0800

    Added GraphCMS config

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ import { ApolloClient } from "apollo-client"
 import { HttpLink } from "apollo-link-http"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import VueApollo from "vue-apollo"
+import { GRAPHCMS_API } from "../config"
+
+const apolloClient = new ApolloClient({
+	link: new HttpLink({ uri: GRAPHCMS_API }),
+	cache: new InMemoryCache(),
+})
+Vue.use(VueApollo)
 
 Vue.use(Buefy, {
 	defaultIconPack: "fas",
